a32da5e0c99cc08eced6b47766e636e21ad89cae
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-10 16:23:49 -0500

    examples

--- API_Relationships.js
+++ API_Relationships.js
@@ -1,17 +1,12 @@
-// Create a quick demonstration of the topic. Be sure to comment out your code and take good notes. Then you will teach back your topic to your group. First, please discuss who will cover which topic so there are no duplicates within your group. There are plenty to choose from! 
+// Create a quick demonstration of the topic. Be sure to comment out your code and take good notes. Then you will teach back your topic to your group. First, please discuss who will cover which topic so there are no duplicates within your group. There are plenty to choose from!  
 // 
-/* 
-FIELDS: 
-    A resource object’s attributes and its relationships are collectively called its “fields”.
-    Fields for a resource object MUST share a common namespace with each other and with type and id. In other words, a resource can not have an attribute and relationship with the same name, nor can it have an attribute or relationship named type or id. 
-*/
-// Example: 
+// FIELDS: An object's attribute plus its relationship
+// 
+// ATTRIBUTES: Any valid JSON value
+// E.g.,: Number, string, boolean, a nested array
 //
-// ATTRIBUTES: 
-// Example: 
+// RELATIONSHIPS: How one object is tied to another object
+// E.g.,: Links, data, or meta
 //
-// RELATIONSHIPS: 
-// Example: 
-//
-// RELATED RESOURCE LINKS: 
-// Example: 
+// RELATED RESOURCE LINKS: Using the link between objects to return a *different* relationship
+// E.g,: An article's "comments" relationship could specify a link that returns a collection of comment resource objects when retrieved 
