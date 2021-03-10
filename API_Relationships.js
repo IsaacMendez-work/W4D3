d5625e012a54499efeecdf60dfacf0cc1a946662
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-10 14:44:42 -0500

    adding json notes

---
+++ API_Relationships.js
@@ -0,0 +1,17 @@
+// Create a quick demonstration of the topic. Be sure to comment out your code and take good notes. Then you will teach back your topic to your group. First, please discuss who will cover which topic so there are no duplicates within your group. There are plenty to choose from! 
+// 
+/* 
+FIELDS: 
+    A resource object’s attributes and its relationships are collectively called its “fields”.
+    Fields for a resource object MUST share a common namespace with each other and with type and id. In other words, a resource can not have an attribute and relationship with the same name, nor can it have an attribute or relationship named type or id. 
+*/
+// Example: 
+//
+// ATTRIBUTES: 
+// Example: 
+//
+// RELATIONSHIPS: 
+// Example: 
+//
+// RELATED RESOURCE LINKS: 
+// Example: 
